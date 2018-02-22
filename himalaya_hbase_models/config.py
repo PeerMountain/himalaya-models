46fe46a2a512e3bc8f2b768299d950ac3bd5fff7
Jonatas Baldin <jonatas.baldin@gmail.com>
2018-02-22 17:15:15 -0500

    Set HABSE_PORT defaults to 8020

--- himalaya_hbase_models/config.py
+++ himalaya_hbase_models/config.py
@@ -2,7 +2,7 @@ from os import getenv
 
 
 HBASE_HOSTNAME=getenv('HBASE_HOSTNAME')
-HBASE_PORT=int(getenv('HBASE_PORT', 9090))
+HBASE_PORT=int(getenv('HBASE_PORT', 8020))
 
 
 if not HBASE_HOSTNAME or not HBASE_PORT:
